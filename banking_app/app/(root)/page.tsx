3ad96c18a38dabe34bea66128b0b0a186e818c70
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-05-08 19:53:47 +0530

    headerbox

--- banking_app/app/(root)/page.tsx
+++ banking_app/app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = {firstName: 'Adrian'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and mange your account and transactions efficiently"
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
